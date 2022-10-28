d49b82b427d1d1cda7c3411d16b63f08b9534ba0
VladGridin <114282482+VladGridin@users.noreply.github.com>
2022-10-28 16:00:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,9 @@ let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
+	tg.MainButton.setText("Вы выбрали товар 1!");
+	item = "1";
+	tg.MainButton.show();
 	}
 });
 
